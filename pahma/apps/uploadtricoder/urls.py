d5c863b38dacd26fbb420f0c5eb9ca91cc5780dc
John Lowe <jblowe@berkeley.edu>
2019-08-01 11:27:08 -0700

    CSW-101: make tricoder urls unique: django2 requires this

--- pahma/apps/uploadtricoder/urls.py
+++ pahma/apps/uploadtricoder/urls.py
@@ -5,10 +5,8 @@ from uploadtricoder import views
 
 urlpatterns = [
     path('', views.uploadfiles),
-    path('uploadfiles', views.uploadfiles, name='uploadfiles'),
-    path('rest/(P<action>[\w\-\.]+)', views.rest, name='rest'),
-    path('checkfilename', views.checkfilename, name='checkfilename'),
-    path('showqueue', views.showqueue, name='showqueue'),
-    path('showresults', views.showresults, name='showresults'),
-    # path(''createtricoder', views.createtricoder, name='createtricoder'),
+    path('tri_uploadfiles', views.uploadfiles, name='uploadfiles'),
+    path('tri_checkfilename', views.checkfilename, name='checkfilename'),
+    path('tri_showqueue', views.showqueue, name='showqueue'),
+    path('tri_showresults', views.showresults, name='showresults'),
 ]
