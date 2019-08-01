d2111c5b2e13e92a431422f0bc8a2d6343dbe5cc
John Lowe <jblowe@berkeley.edu>
2019-08-01 14:35:46 -0700

    CSW-101: correct path in x3dviewer

--- pahma/apps/x3dviewer/urls.py
+++ pahma/apps/x3dviewer/urls.py
@@ -4,6 +4,5 @@ from django.urls import include, path
 from x3dviewer import views
 
 urlpatterns = [
-    path('', views.x3dviewer, name='x3dviewer'),
-    path('(P<md5>[\w\-\.]+)?', views.x3dviewer, name='x3dviewer')
+    path('', views.x3dviewer, name='x3dviewer')
 ]
