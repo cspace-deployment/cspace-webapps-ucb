50fc2432e9784727865453ed8cd7bb229f750499
John Lowe <jblowe@berkeley.edu>
2019-09-13 07:56:49 -0700

    CSW-118: refactor csvImport shell scripts to support multiple record types

--- ucjeps/apps/csvimport/add.sh
+++ ucjeps/apps/csvimport/add.sh
@@ -5,6 +5,6 @@ set -x
 rm -f $1.runlog.out
 grep -v Insec $1.runlog.out > $1.validate.log; rm $1.runlog.out
 touch $1.inprogress.log
-time $PYTHON DWC2CSpace.py $1.add.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.add-audit.csv /dev/null /dev/null add $2 > $1.runlog.out 2>&1
+time $PYTHON DWC2CSpace.py $1.add.csv ../config/csvimport.cfg ../config/csvimport.$2.definitions.csv ../config/csvimport.$2.template.xml $1.add-audit.csv /dev/null /dev/null add $2 > $1.runlog.out 2>&1
 grep -v Insec $1.runlog.out > $1.added.log; rm $1.runlog.out
 cat $1.inprogress.log >> $1.runstatistics.log ; rm $1.inprogress.log
--- ucjeps/apps/csvimport/count.sh
+++ ucjeps/apps/csvimport/count.sh
@@ -4,7 +4,7 @@ PYTHON=/var/www/venv/bin/python
 set -x
 rm -f $1.runlog.out
 touch $1.inprogress.log
-time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.count.csv /dev/null /dev/null count $2 > $1.runlog.out 2>&1
+time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/csvimport.$2.definitions.csv ../config/csvimport.$2.template.xml $1.count.csv /dev/null /dev/null count $2 > $1.runlog.out 2>&1
 grep -v Insec $1.runlog.out > $1.counted.log; rm $1.runlog.out
 cat $1.inprogress.log >> $1.runstatistics.log ; rm $1.inprogress.log
 
