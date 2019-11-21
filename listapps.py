ad487bb3576d7d4fae2f804f5515590ac4c3d36b
John Lowe <jblowe@berkeley.edu>
2019-11-21 05:42:27 -0800

    CSW-191: initial workaround for 'regular UI' links: just eliminate them

--- listapps.py
+++ listapps.py
@@ -167,9 +167,8 @@ header = """
 cell = """
 <tr>
 <td style="width: 340px; vertical-align: top;">
-<a  class="likeabutton" target="_blank" href="https://%s.cspace.berkeley.edu">
 <h3>%s</h3>
-<img style="max-width: 330px ; max-height: 100px" alt="%s" src="https://webapps.cspace.berkeley.edu/%s_static/cspace_django_site/images/header-logo.png"></a>
+<img style="max-width: 330px ; max-height: 100px" alt="%s" src="https://webapps.cspace.berkeley.edu/%s_static/cspace_django_site/images/header-logo.png">
 </td>
 """
 
@@ -285,7 +284,8 @@ elif output_type == 'html':
         html += wrap('td', wrap('b', app_type)).replace('<td>', '<td style="width: 300px;">')
     html += '</tr>'
     for tenant in tenants:
-        html += cell % (tenant, MUSEUMS[tenant][0], MUSEUMS[tenant][0], tenant)
+        # html += cell % (tenant, MUSEUMS[tenant][0], MUSEUMS[tenant][0], tenant)
+        html += cell % (MUSEUMS[tenant][0], MUSEUMS[tenant][0], tenant)
         for app_type in 'Public Private'.split(' '):
             html += '<td style="vertical-align: top; padding-top: 20px;">'
             for app in sorted(all_apps.keys()):
