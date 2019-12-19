629c20566967db7ba13960f3ddad85b8d80e43c9
John Lowe <jblowe@berkeley.edu>
2019-12-19 15:56:30 -0800

    CSW-202: example 'undo' script should point to production

--- ucjeps/apps/csvimport/undo.sh
+++ ucjeps/apps/csvimport/undo.sh
@@ -3,7 +3,7 @@ set -x
 cd $( dirname "${BASH_SOURCE[0]}" )
 rm -f $1.runlog.out
 touch $1.inprogress.log
-source set-config-ucjeps-dev.sh
+source set-config-ucjeps-prod.sh
 if [ -e $1.*-audit.csv ]; then
     # create a list of csids, then use the delete script to delete them
     cut -f2 $1.*-audit.csv > $1.csids.csv
