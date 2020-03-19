97b0482deb7365b05b240b0e6e59c365c4eaf314
John Lowe <jblowe@berkeley.edu>
2020-03-19 13:11:46 -0700

    CSW-270: minor refactoring of this cinefiles pdf helper script

--- cinefiles/apps/uploadmedia/make-step1.py
+++ cinefiles/apps/uploadmedia/make-step1.py
@@ -1,19 +1,19 @@
-import os, sys
+import os, sys, time
 
 header = 'name|size|objectnumber|date|creator|contributor|rightsholder|imagenumber|handling|approvedforweb|description'
-template = 'f{name}|f{size}|f{doc_id}||||||f{handling}|on|f{description}'
 
 description = 'automatically generated pdf'
 handling = ''
 size = ''
+date = time.strftime("%Y/%m/%dT%H:%M:%S", time.localtime())
 
 rootDir = sys.argv[1]
-for dirName, subdirList, fileList in os.walk(rootDir):
+for dirName, subdirList, fileList in sorted(os.walk(rootDir)):
     print('directory: %s' % dirName)
-    job_file = open(f'/tmp/bmu/{dirName}.step1.csv', 'w')
-    job_file.write(header)
-    for name in fileList:
+    job_file = open(f'{rootDir}/{dirName}.step1.csv', 'w')
+    job_file.write(header + '\n')
+    for name in sorted(fileList):
         doc_id = name.replace('.pdf', '')
-        job_file.write(template)
+        job_file.write(f'{name}|{size}|{doc_id}|{date}|||||{handling}|on|{description}\n')
         print('\t%s' % name)
     job_file.close()
