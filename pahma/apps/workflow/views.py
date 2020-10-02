521487e0c39488a473b3ab1df112c21277f0d687
John Lowe <jblowe@berkeley.edu>
2020-10-02 13:49:36 -0700

    CSW-356: fix workflow pagination for pahma

--- pahma/apps/workflow/views.py
+++ pahma/apps/workflow/views.py
@@ -2,6 +2,7 @@ __author__ = 'jblowe'
 
 import re
 import logging
+import urllib
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -49,9 +50,8 @@ def workflow(request):
         end_date_timestamp = end_date.strip() + "T23:59:59"
         connection = cspace.connection.create_connection(config, request.user)
         search_terms = 'as=collectionspace_core:updatedAt >= TIMESTAMP "%s" AND collectionspace_core:updatedAt <= TIMESTAMP "%s"'
-        #search_terms = urllib.parse.quote_plus(search_terms % (start_date, end_date))
-        search_terms = search_terms % (start_date_timestamp, end_date_timestamp)
-        search_terms = search_terms.replace(' ', '%20')
+        search_terms = search_terms % (start_date, end_date)
+        search_terms = urllib.parse.quote_plus(search_terms)
         logger.info('%s :: %s' % ('workflow', 'cspace-services/%s?%s&pgSz=%s&wf_deleted=false&pgNum=%s' % ('collectionobjects', search_terms, size_limit, int(page-1))))
         (url, data, statusCode,elapsedtime) = connection.make_get_request('cspace-services/%s?%s&pgSz=%s&wf_deleted=false&pgNum=%s' % ('collectionobjects', search_terms, size_limit, int(page-1)))
         # ...collectionobjects?kw=%27orchid%27&wf_deleted=false
