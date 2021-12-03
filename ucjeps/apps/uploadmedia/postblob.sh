e2d1c90d95f4cdff92751692d93bf007a9e3a704
John Lowe <jblowe@berkeley.edu>
2021-12-03 09:48:58 -0800

    CSW-553: ucjeps bmu converts cr2s to jpgs, not pngs, for now

--- ucjeps/apps/uploadmedia/postblob.sh
+++ ucjeps/apps/uploadmedia/postblob.sh
@@ -61,11 +61,11 @@ grep -i '\.CR2' $INPUTFILE | cut -f1 -d"|" > CR2file
 for CR2 in `cat CR2file`
 do
    F=$(echo "$CR2" | sed "s/\.CR2/_CR2/i")
-   echo "converting ${CR2} to ${F}.PNG" >> $TRACELOG
-   convert -verbose ${IMGDIR}/${CR2} ${IMGDIR}/${F}.PNG >> $TRACELOG 2>&1
+   echo "converting ${CR2} to ${F}.JPG" >> $TRACELOG
+   convert -verbose ${IMGDIR}/${CR2} ${IMGDIR}/${F}.JPG >> $TRACELOG 2>&1
 done
 # change the file names in the bmu job file so that it will upload the PNGs
-perl -i -pe 's/\.CR2/_CR2.PNG/i' $INPUTFILE
+perl -i -pe 's/\.CR2/_CR2.JPG/i' $INPUTFILE
 
 trace "python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG"
 python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG 2>&1
