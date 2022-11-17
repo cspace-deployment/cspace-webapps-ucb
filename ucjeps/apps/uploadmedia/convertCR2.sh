23b7536a03e78a4b0154a74e6ddf5fd749b0ba2f
John Lowe <jblowe@berkeley.edu>
2022-11-17 05:58:39 -0800

    CSW-725: handle several special (orientation) cases in ucjeps bmu

---
+++ ucjeps/apps/uploadmedia/convertCR2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# these better be here!
+source ~/.profile
+source ${HOME}/venv/bin/activate
+
+CR2="$1"
+
+fileinfo $CR2
+echo
+
+F=$(echo "$CR2" | sed "s/\.CR2//i")
+# make a jpg and a tif for each cr2
+for FORMAT in JPG TIF
+do
+  echo "converting ${CR2} to ${F}.${FORMAT}"
+  convert -verbose "${CR2}" "${F}.${FORMAT}" 2>&1
+  echo ">>>> after initial conversion"
+  fileinfo "${F}.${FORMAT}"
+  echo
+  # if the image (TIF or JPG) is still landscape, rotate it 90 CCW
+  if [[ "1" == `convert "${F}.${FORMAT}" -format "%[fx:(w/h>1)?1:0]" info:` ]]
+  then
+     ROTATION="-90"
+     if [[ `exiftool "${CR2}" | grep "Rotate 90 CW"` ]]
+     then
+        ROTATION="+90"
+     fi
+     echo "${F}.${FORMAT}" is Landscape, rotating ${ROTATION}
+     echo convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
+     convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
+     fileinfo "${F}.${FORMAT}"
+     echo
+  fi
+done
+echo
--- ucjeps/apps/uploadmedia/postblob.sh
+++ ucjeps/apps/uploadmedia/postblob.sh
@@ -61,7 +61,7 @@ grep -i '\.CR2' $INPUTFILE | cut -f1 -d"|" > CR2file
 # convert them all
 for CR2 in `cat CR2file`
   do
-    F=$(echo "$CR2" | sed "s/\.CR2/_CR2/i")
+    F=$(echo "$CR2" | sed "s/\.CR2//i")
     # fetch the CR2 from S3
     echo "${RUNDIR}/cps3.sh \"$CR2\" ucjeps from" >> $TRACELOG
     ${RUNDIR}/cps3.sh "$CR2" ucjeps from >> $TRACELOG 2>&1
@@ -70,6 +70,13 @@ for CR2 in `cat CR2file`
     do
       echo "converting ${CR2} to ${F}.${FORMAT}" >> $TRACELOG
       convert -verbose "/tmp/${CR2}" "/tmp/${F}.${FORMAT}" >> $TRACELOG 2>&1
+      # if the image (TIF or JPG) is still landscape, rotate it 90 CCW
+      if [[ `convert "/tmp/${F}.${FORMAT}" -format "%[fx:(w/h>1)?1:0]" info:` ]]
+      then
+         echo "/tmp/${F}.${FORMAT}" is Landscape, rotating 90 CCW >> $TRACELOG
+         echo convert -rotate -90 "/tmp/${F}.${FORMAT}" "/tmp/${F}.${FORMAT}" >> $TRACELOG
+         convert -rotate -90 "/tmp/${F}.${FORMAT}" "/tmp/${F}.${FORMAT}"
+      fi
       # put the converted file back into S3
       echo "${RUNDIR}/cps3.sh \"${F}.${FORMAT}\" ucjeps to" >> $TRACELOG
       ${RUNDIR}/cps3.sh "${F}.${FORMAT}" ucjeps to >> $TRACELOG 2>&1
@@ -77,9 +84,9 @@ for CR2 in `cat CR2file`
   rm "/tmp/${CR2}"
 done
 # change the file names in the bmu job file so that it will upload the JPGs
-perl -i -pe 's/\.CR2/_CR2.JPG/i' $INPUTFILE
+perl -i -pe 's/\.CR2/.JPG/i' $INPUTFILE
 # make a copy of the input file suitable for input to the merritt archiving system
-perl -pe 's/\.CR2/_CR2.TIF/i' $INPUTFILE > $JOB.archive.csv
+perl -pe 's/\.CR2/.TIF/i' $INPUTFILE > $JOB.archive.csv
 
 trace "python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG"
 python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG 2>&1
