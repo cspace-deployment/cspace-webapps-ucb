edaf6092215910cbe7fe426e475948dd759d1278
John Lowe <jblowe@berkeley.edu>
2023-03-24 08:30:00 -0700

    CSW-817: status script should use configured database, not hardcoded one

--- ucjeps/apps/merritt_archive/code/batch/status.sh
+++ ucjeps/apps/merritt_archive/code/batch/status.sh
@@ -3,7 +3,9 @@
 echo "records in ucjeps archiving transaction database, counts by type"
 echo
 
-sqlite3 /var/www/ucjeps/db.sqlite3 "select CURRENT_DATE,status,count(*) from merritt_archive_transaction group by status;" | perl -pe 's/\|/\t/g' | expand -12
+source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
+
+sqlite3 ${SQLITE3_DB} "select CURRENT_DATE,status,count(*) from merritt_archive_transaction group by status;" | perl -pe 's/\|/\t/g' | expand -12
 
 echo
 echo "jobs completed as of today `date`"
