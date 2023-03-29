f12c0b6466b938222db9f68570e02f78542109a5
John Lowe <jblowe@berkeley.edu>
2023-03-29 08:42:25 -0700

    CSW-817: next version of 'orientation detection script

--- ucjeps/apps/merritt_archive/code/batch/check_orientation.sh
+++ ucjeps/apps/merritt_archive/code/batch/check_orientation.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 IMAGE="$1"
+WIDTH=250
 
-magick "${IMAGE}" -crop 4000x200+0+0 "${IMAGE}.top.jpg"
-magick "${IMAGE}" -gravity South -crop 4000x200+0+0 -rotate +180 "${IMAGE}.bottom.jpg"
-top=`magick compare -channel red -metric MAE colorbar.jpg "${IMAGE}.top.jpg" z.jpg 2>&1 | perl -ne 'chomp; s/[\. ].*// ; print $_;'`
-bot=`magick compare -channel red -metric MAE colorbar.jpg "${IMAGE}.bottom.jpg" z.jpg 2>&1 | perl -ne 'chomp; s/[\. ].*// ; print $_;'`
+convert "${IMAGE}" -crop 4000x${WIDTH}+0+0 "${IMAGE}.top.jpg"
+convert "${IMAGE}" -gravity South -crop 4000x${WIDTH}+0+0 -rotate +180 "${IMAGE}.bottom.jpg"
+top=`compare -channel red -metric MAE colorbar.top.jpg "${IMAGE}.top.jpg" temp.jpg 2>&1 | perl -ne 'chomp; s/[\. ].*// ; print $_;'`
+bot=`compare -channel red -metric MAE colorbar.bottom.jpg "${IMAGE}.bottom.jpg" temp.jpg 2>&1 | perl -ne 'chomp; s/[\. ].*// ; print $_;'`
 if [[ $bot -gt $top ]] ; then
   orientation="OK"
 else
   orientation="UpsideDown"
 fi
 echo "$orientation $top $bot"
+rm "${IMAGE}.top.jpg" "${IMAGE}.bottom.jpg" temp.jpg
