094621a04b2793e6e64de68b7ab6c1cf954ccc34
John Lowe <jblowe@berkeley.edu>
2023-04-05 14:20:00 -0700

    CSW-817: ucjeps archiving sqlite3 database location revision

--- ucjeps/apps/merritt_archive/code/batch/make_backup.sh
+++ ucjeps/apps/merritt_archive/code/batch/make_backup.sh
@@ -3,6 +3,7 @@
 source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
 
 export RUN_DATE=`date +%Y-%m-%d-%H-%M`
+DB_NAME=`basename ${SQLITE3_DB}`
 # make a backup
-cp ${SQLITE3_DB} ${SQLITE3_DB}.backup.${RUN_DATE}
-
+echo aws s3 cp ${SQLITE3_DB} ${S3URI}/${DB_NAME}.backup.${RUN_DATE}
+aws s3 cp ${SQLITE3_DB} ${S3URI}/${DB_NAME}.backup.${RUN_DATE}
