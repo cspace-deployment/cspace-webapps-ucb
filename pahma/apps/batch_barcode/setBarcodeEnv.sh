ea9e358b986547b8ada5601b2725d8c8a7861050
John Lowe <jblowe@berkeley.edu>
2023-04-20 11:41:23 -0700

    CSW-838: minor updating of 'example' pahma tricoder config file

--- pahma/apps/batch_barcode/setBarcodeEnv.sh
+++ pahma/apps/batch_barcode/setBarcodeEnv.sh
@@ -1,18 +1,17 @@
 #set verbose
 
 # target server
-export URL="https://pahma-dev.cspace.berkeley.edu/cspace-services/imports"
-export USER="xxx@pahma.cspace.berkeley.edu:xxxx"
+export URL="https://pahma.qa.collectionspace.org/cspace-services/imports"
+export USER="xxxxx@pahma.cspace.berkeley.edu:xxxxx"
 export CONTENT_TYPE="Content-Type: application/xml"
 
 # password comes from .pgpass
-export CONNECTSTRING="host=dba-postgres-dev-45.ist.berkeley.edu port=5117 sslmode=prefer dbname=pahma_domain_pahma user=reporter_pahma "
+export CONNECTSTRING="host=localhost port=54321 sslmode=prefer dbname=pahma_domain_pahma user=reporter_pahma"
 
 # setup for email
 export SUBJECT="Tricoder Upload Results  `date`"
-#export EMAIL="pahma-tricoder@lists.berkeley.edu"
+export EMAIL="pahma-tricoder@lists.berkeley.edu"
 #export EMAIL="mtblack@berkeley.edu"
-export EMAIL="jblowe@berkeley.edu"
 
 export ROOT_PATH=/cspace/batch_barcode
 export UPLOAD_PATH=${ROOT_PATH}/input
