b609c032ecd749905ca767161943e09739186db5
John Lowe <jblowe@berkeley.edu>
2023-04-25 13:59:35 -0700

    CSW-817: use the same convertCR2 function in ucjeps bmu and archiving

--- ucjeps/apps/uploadmedia/convertCR2.sh
+++ ucjeps/apps/uploadmedia/convertCR2.sh
@@ -10,22 +10,20 @@ echo "converting ${CR2} to TIF AND JPG, and making a thumbnail"
 #echo "/usr/bin/dcraw -w -c -compress zip \"${CR2}\" > \"${F}.TIF\""
 #/usr/bin/dcraw -w -c -auto-orient -compress zip "${CR2}" > "${F}.TIF" 2>&1 &
 echo convert -verbose "${CR2}" -auto-orient -depth 8 -compress zip "${F}.TIF"
-echo "convert \"${F}.JPG\" -quality 60 -thumbnail 20% ${F}.JPG.thumbnail.jpg"
 convert -verbose "${CR2}" -auto-orient -depth 8 -compress zip "${F}.TIF" 2>&1 &
 convert -verbose "${CR2}" -auto-orient "${F}.JPG" 2>&1 &
-TMPFILE=$(mktemp /tmp/ucjeps-archiving-temp.XXXXXX)
+TMPFILE="/tmp/${F}.convert.txt"
+# TMPFILE=$(mktemp /tmp/ucjeps-archiving-temp.XXXXXX)
 exiftool "${CR2}" > ${TMPFILE} &
 wait
-# now make the thumbnail from the JPG we just converted
-convert "${F}.JPG" -quality 60 -thumbnail 20% ${F}.JPG.thumbnail.jpg &
 for FORMAT in JPG TIF
 do
   # if the image (TIF or JPG) is still landscape, rotate it as needed
   if [[ "1" == `convert "${F}.${FORMAT}" -format "%[fx:(w/h>1)?1:0]" info:` ]]
   then
      echo "${F}.${FORMAT}" is still Landscape. Checking EXIF data...
-     grep Orientation ${TMPFILE}
-     if $(perl -ne 'print if /^Orientation/' ${TMPFILE} | grep "Horizontal") ; then
+     grep Orientation "${TMPFILE}"
+     if [[ $(grep -e "^Orientation" "${TMPFILE}") =~ Horizontal ]]; then
         ROTATION="+270"
      else
         # default is +90, based on sampling
@@ -35,12 +33,6 @@ do
      convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
   else
      echo "${F}.${FORMAT}" is Portrait.
-     if $(perl -ne 'print if /^Orientation/' ${TMPFILE} | grep "Rotate 270 CW") ; then
-       echo "... but exif data say we still have to flip it."
-       ROTATION="+180"
-       echo convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
-       convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
-     fi
   fi
 
   ORIENTATION=`/cspace/merritt/batch/check_orientation_multi.sh "${F}.${FORMAT}"`
@@ -53,6 +45,9 @@ do
   fi
   echo
 done
+# now make the thumbnail from the JPG we just converted
+echo "convert \"${F}.JPG\" -quality 60 -thumbnail 20% \"${F}.thumbnail.jpg\""
+convert "${F}.JPG" -quality 60 -thumbnail 20% "${F}.thumbnail.jpg" &
 rm ${TMPFILE}
 wait
 echo
