9181bafe366da95b65be9b31efb25066caa79b98
John Lowe <jblowe@berkeley.edu>
2023-05-02 10:20:44 -0700

    CSW-817: minor refactor of exif data checking script for ucjeps archving

--- ucjeps/apps/merritt_archive/code/batch/stats.sh
+++ ucjeps/apps/merritt_archive/code/batch/stats.sh
@@ -8,8 +8,7 @@ fi
 tmpfile=$(mktemp /tmp/ucjeps-archiving-temp.XXXXXX)
 
 exiftool $1 > ${tmpfile}
-grep Orient ${tmpfile} | perl -pe 's/             //' | head -1
-grep "Camera Orient" ${tmpfile} | perl -pe 's/             //' | head -1
+grep "Orientation" ${tmpfile} | perl -pe 's/             //' | head -1
 grep "Image Width" ${tmpfile} | perl -pe 's/             //' | head -1
 grep "Image Height" ${tmpfile} | perl -pe 's/             //' | head -1
 grep "Camera Model Name" ${tmpfile} | perl -pe 's/             //' | head -1
