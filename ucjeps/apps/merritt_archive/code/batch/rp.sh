a12ba89ab1e0b99ed7d944bb25460c17b73c44c4
John Lowe <jblowe@berkeley.edu>
2023-05-12 16:01:30 -0700

    CSW-817: additional helper script for ucjeps archiving pipeline

---
+++ ucjeps/apps/merritt_archive/code/batch/rp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
+
+JOB="$1"
+
+cd /cspace/merritt/batch
+
+if [[ ! -f "/cspace/merritt/jobs/${JOB}.input.csv" ]]; then
+  echo "${JOB}.input.csv does not exist; exiting."
+  exit 1
+fi
+
+ ./run_pipeline.sh /cspace/merritt/jobs/${JOB}.input.csv | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' > /cspace/merritt/jobs/${JOB}.log 2>&1
+
