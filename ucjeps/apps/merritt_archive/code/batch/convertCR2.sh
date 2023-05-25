2472ce8a599757d3989576b7b4acede7c4729c7d
John Lowe <jblowe@berkeley.edu>
2023-05-25 11:17:26 -0700

    CSW-817: latest improvements to convertCR2.sh

--- ucjeps/apps/merritt_archive/code/batch/convertCR2.sh
+++ ucjeps/apps/merritt_archive/code/batch/convertCR2.sh
@@ -3,25 +3,59 @@
 # set -xv
 set -o errexit
 
-source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
+# exif value for orientation are 1-8 with 1 meaning "Horizontal" (i.e. no rotation)
+function detect_orientation() {
+  case $1 in
+    2)
+    ROTATION='-flip horizontal'
+    ;;
+    3)
+    ROTATION='-rotate 180'
+    ;;
+    4)
+    ROTATION='-flip vertical'
+    ;;
+    5)
+    ROTATION='-transpose'
+    ;;
+    6)
+    ROTATION='-rotate 90'
+    ;;
+    7)
+    ROTATION='-transverse'
+    ;;
+    8)
+    ROTATION='-rotate 270'
+    ;;
+    *)
+    ROTATION=''
+    ;;
+  esac
+  echo "$ROTATION"
+}
+
+# os-specific command to format output of 'time'
+export TIME_COMMAND="/usr/bin/time -f TIME,%E,%U,%S,%C"
 
 CR2="$1"
+DCRAW_PARMS="-a"
 
-F=$(echo "$CR2" | sed "s/\.CR2//i")
+F=${CR2/.CR2/}
 # make a jpg and a tif for each cr2
 echo "converting ${CR2} to TIF AND JPG, and making a thumbnail"
-echo /usr/bin/dcraw -v -a "${CR2}"
 # we first convert to ppm...
-${TIME_COMMAND} /usr/bin/dcraw -v -a "${CR2}"
+${TIME_COMMAND} /usr/bin/dcraw -v "${DCRAW_PARMS}" "${CR2}"
 # ...then to 8-bit tif
 # nb: we do the compressing later after we finish fussing with the tif
-echo convert -verbose ${F}.ppm -depth 8 "${F}.TIF"
 ${TIME_COMMAND} convert -verbose ${F}.ppm -depth 8 "${F}.TIF"
+rm ${F}.ppm
 #echo convert -verbose "${CR2}" -auto-orient -depth 8 -compress zip "${F}.TIF"
 #convert -verbose "${CR2}" -auto-orient -depth 8 -compress zip "${F}.TIF" 2>&1 &
 TMPFILE="${F}.exifdata.txt"
-echo exiftool "${CR2}" > ${TMPFILE}
-${TIME_COMMAND} exiftool "${CR2}" > ${TMPFILE}
+${TIME_COMMAND} exiftool "${CR2}" > ${TMPFILE} &
+TMPFILE_NUMERIC="${F}.exifdata_numeric.txt"
+${TIME_COMMAND} exiftool -n "${CR2}" > ${TMPFILE_NUMERIC} &
+wait
 for FORMAT in TIF
 do
   # if the image (TIF or JPG) is still landscape, rotate it as needed
@@ -29,43 +63,34 @@ do
   if [[ "1" == "${LANDSCAPE}" ]]
   then
      echo "${F}.${FORMAT}" is still Landscape. Checking EXIF data...
-     grep Orientation "${TMPFILE}"
-     if [[ ! $(grep -e "^Orientation" "${TMPFILE}") =~ Horizontal ]]; then
-        # default is +90, based on sampling
-        echo "EXIF orientation is 'Horizontal', rotating +90"
-        ROTATION="+90"
-      else
-        # rotate +90 anyway
-        echo rotate +90 anyway
-        ROTATION="+90"
-     fi
-     echo convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
-     ${TIME_COMMAND} convert -rotate ${ROTATION} "${F}.${FORMAT}" "${F}.${FORMAT}"
+     EXIF_ORIENTATION=$(grep -e "^Orientation" ${TMPFILE})
+     echo "EXIF_ORIENTATION: ${EXIF_ORIENTATION}"
+     ORIENTATION_NUMERIC=$(grep -e "^Orientation" ${TMPFILE_NUMERIC} | head -1 | perl -pe 's/.*(\d+).*/\1/')
+     ROTATION=$(detect_orientation ${ORIENTATION_NUMERIC})
+     echo "${ORIENTATION_NUMERIC} => ${ROTATION}"
+     echo convert "${F}.${FORMAT}" ${ROTATION} "${F}.${FORMAT}"
+     ${TIME_COMMAND} convert "${F}.${FORMAT}" ${ROTATION} "${F}.${FORMAT}"
   else
      echo "${F}.${FORMAT}" is Portrait.
   fi
 
   ORIENTATION=`/cspace/merritt/batch/check_orientation_multi.sh "${F}.${FORMAT}"`
-  echo "orientation: ${ORIENTATION}"
+  echo "detected orientation: ${ORIENTATION}"
   echo "landscape: ${LANDSCAPE}"
-  # if we already rotated a landscape image, don't bother checking its orientation
-  if [[ ${ORIENTATION} =~ UpsideDown && "0" == "${LANDSCAPE}" ]]
+  if [[ ${ORIENTATION} =~ UpsideDown ]]
   then
     echo "looks like it is still upside down. rotating +180."
-    echo convert -rotate +180 "${F}.${FORMAT}" "${F}.${FORMAT}"
     ${TIME_COMMAND} convert -rotate +180 "${F}.${FORMAT}" "${F}.${FORMAT}"
   else
-    echo "classifier says image is upright (or we already tipped it from landscape)."
+    echo "classifier says image is upright."
   fi
 done
 # compress the tif
-${TIME_COMMAND} convert -verbose ${F}.TIF -compress zip "${F}.TIF"
+${TIME_COMMAND} convert -verbose ${F}.TIF -compress zip "${F}.TIF" &
 # make the JPG from the TIF we just converted
-echo convert -verbose "${F}.TIF" "${F}.JPG"
-convert -verbose "${F}.TIF" "${F}.JPG"
-# now make the thumbnail from the JPG
-echo "convert \"${F}.JPG\" -quality 60 -thumbnail 20% \"${F}.thumbnail.jpg\""
-${TIME_COMMAND} convert "${F}.JPG" -quality 60 -thumbnail 20% "${F}.thumbnail.jpg"
+${TIME_COMMAND} convert -verbose "${F}.TIF" "${F}.JPG" &
+wait
 # we keep the exifdata file for now; another process takes care of it
 # rm ${TMPFILE}
+rm ${TMPFILE_NUMERIC}
 echo
