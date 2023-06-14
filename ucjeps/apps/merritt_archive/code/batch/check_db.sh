5e0ad833f4f6391f18f1de725f86fc99305c3c57
John Lowe <jblowe@berkeley.edu>
2023-06-13 17:05:38 -0700

    CSW-817: add -v option to ucjeps archiving script 'check_db.sh'

--- ucjeps/apps/merritt_archive/code/batch/check_db.sh
+++ ucjeps/apps/merritt_archive/code/batch/check_db.sh
@@ -5,6 +5,11 @@ TRANSACTION_TYPE="$2"
 ACCESSION_NUMBER="${IMAGE_FILE_NAME/_*/}"
 ACCESSION_NUMBER="${ACCESSION_NUMBER/.*/}"
 
-RESULT=$(curl -S -s "http://localhost:8983/solr/ucjeps-merritt/select?indent=true&q.op=OR&q=accession_number_s%3A${ACCESSION_NUMBER}%20AND%20status_s:${TRANSACTION_TYPE}&rows=9")
-
-echo $RESULT | jq -r '.response.docs[0].transaction_detail_s'
+if [[ "$3" == "-v" ]]
+then
+  RESULT=$(curl -S -s "http://localhost:8983/solr/ucjeps-merritt/select?indent=true&q.op=OR&q=accession_number_s%3A${ACCESSION_NUMBER}&rows=100")
+  echo $RESULT | jq
+else
+  RESULT=$(curl -S -s "http://localhost:8983/solr/ucjeps-merritt/select?indent=true&q.op=OR&q=accession_number_s%3A${ACCESSION_NUMBER}%20AND%20status_s:${TRANSACTION_TYPE}&rows=1")
+  echo $RESULT | jq -r '.response.docs[0].transaction_detail_s'
+fi
