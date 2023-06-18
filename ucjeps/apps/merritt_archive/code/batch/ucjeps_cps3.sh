e4b2f757d2cf4834c808966c7962f6f1666da48c
John Lowe <jblowe@berkeley.edu>
2023-06-18 11:01:15 -0700

    CSW-817: source bucket location is now always the 'in transit' bucket, set in env. vars

--- ucjeps/apps/merritt_archive/code/batch/ucjeps_cps3.sh
+++ ucjeps/apps/merritt_archive/code/batch/ucjeps_cps3.sh
@@ -9,18 +9,11 @@ if [[ $# -ne 3 ]] ; then
   exit 1
 fi
 
-BL_ENVIRONMENT=`/usr/bin/curl -s -m 5 http://169.254.169.254/latest/meta-data/tags/instance/BL_ENVIRONMENT`
-if [[ -z $BL_ENVIRONMENT || ( "$BL_ENVIRONMENT" != "dev" && "$BL_ENVIRONMENT" != "qa" && "$BL_ENVIRONMENT" != "prod" ) ]]; then
-	echo "Could not fetch BL_ENVIRONMENT, are you sure you're on AWS?"
-	exit 1
-fi
-
 FILEPATH="$1"
 FILENAME=`basename -- "${FILEPATH}"`
 MUSEUM="$2"
 DIRECTION="$3"
-MERRITT_TRANSIT="cspace-merritt-in-transit-${BL_ENVIRONMENT}"
-# NB: UCJEPS_BUCKET must be set as environment variable
+MERRITT_TRANSIT="${S3URI}"
 
 echo "copying ${FILENAME} ${DIRECTION} S3  ..."
 
@@ -33,18 +26,18 @@ if [[ "${DIRECTION}" == "to" ]] ; then
     exit 1
   fi
   for i in {1..1}; do
-    echo "/usr/bin/aws s3 cp --quiet '/tmp/${FILENAME}' 's3://${MERRITT_TRANSIT}/${FILENAME}'"
-    ${TIME_CMD} /usr/bin/aws s3 cp --quiet "/tmp/${FILENAME}" "s3://${MERRITT_TRANSIT}/${FILENAME}" && s=0 && break || s=$?
+    echo "/usr/bin/aws s3 cp --quiet '/tmp/${FILENAME}' '${MERRITT_TRANSIT}/${FILENAME}'"
+    ${TIME_CMD} /usr/bin/aws s3 cp --quiet "/tmp/${FILENAME}" "${MERRITT_TRANSIT}/${FILENAME}" && s=0 && break || s=$?
     echo "failed with exit code $s. retrying. attempt $i"
   done
   rm -f "/tmp/${FILENAME}"
   exit $s
 elif [[ "${DIRECTION}" == "from" ]] ; then
   for i in {1..1}; do
-    echo "/usr/bin/aws s3 cp --quiet '${UCJEPS_BUCKET}/${FILEPATH}' /tmp/${FILENAME}"
-    ${TIME_CMD} /usr/bin/aws s3 cp --quiet "${UCJEPS_BUCKET}/${FILEPATH}" /tmp/${FILENAME}
+    echo "/usr/bin/aws s3 cp --quiet '${MERRITT_TRANSIT}/${FILEPATH}' /tmp/${FILENAME}"
+    ${TIME_CMD} /usr/bin/aws s3 cp --quiet "${MERRITT_TRANSIT}/${FILEPATH}" /tmp/${FILENAME}
     if [[ $(head -1 /tmp/${FILENAME} | grep 'not found in S3 bucket') ]] ; then
-      echo "${FILENAME} not found in S3 bucket ${UCJEPS_BUCKET}"
+      echo "${FILENAME} not found in S3 bucket ${MERRITT_TRANSIT}"
       exit 1
     fi
     [ -e "/tmp/${FILENAME}" ] && exit 0
