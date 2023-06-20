436cd156e767f236e8620ae5bacb143d90de16f8
John Lowe <jblowe@berkeley.edu>
2023-06-20 11:28:20 -0700

    CSW-817: this script needs to access the ucjeps merritt archive solr core

--- ucjeps/apps/merritt_archive/code/batch/extract_archived_images.sh
+++ ucjeps/apps/merritt_archive/code/batch/extract_archived_images.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-#
 
 source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
 
-echo "extracting archived image data from sqlite3 database ..."
-sqlite3 ${SQLITE3_DB}  << HERE
-.mode tabs
-select * from merritt_archive_transaction where status = 'archived';
-HERE
+curl -S -s "${SOLR_SERVER}/solr/ucjeps-merritt/select?q=status_s%3Aarchived&fl=image_filename_s&rows=1000000" | jq -r '.response.docs[].image_filename_s'
