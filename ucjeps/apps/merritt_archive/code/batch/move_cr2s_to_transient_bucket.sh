31fe8d8d1dd70dae7b5aa2e26d4fe9e66978a575
John Lowe <jblowe@berkeley.edu>
2023-06-20 11:30:29 -0700

    CSW-817: ucjeps imaging pipeline needs to be able to handle different snowcone buckets

--- ucjeps/apps/merritt_archive/code/batch/move_cr2s_to_transient_bucket.sh
+++ ucjeps/apps/merritt_archive/code/batch/move_cr2s_to_transient_bucket.sh
@@ -5,17 +5,18 @@
 set -o errexit
 source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
 
-if [[ $# -ne 1 ]] ; then
-  echo "Usage: $0 inputfile"
+if [[ $# -ne 2 ]] ; then
+  echo "Usage: $0 inputfile snowcone"
   exit 1
 fi
 
 INPUTFILE="$1"
+SNOWCONE="$2"
 
 while IFS=$'\t' read -r CR2
   do
     CR2_FILENAME=`basename -- "${CR2}"`
-    echo aws s3 cp --quiet \"${UCJEPS_BUCKET}/${CR2}\" \"${S3URI}/${CR2_FILENAME}\"
-    aws s3 cp --quiet "${UCJEPS_BUCKET}/${CR2}" "${S3URI}/${CR2_FILENAME}"
+    echo aws s3 cp --quiet \"s3://${SNOWCONE}/${CR2}\" \"${S3URI}/${CR2_FILENAME}\"
+    aws s3 cp --quiet "s3://${SNOWCONE}/${CR2}" "${S3URI}/${CR2_FILENAME}"
   done < ${INPUTFILE}
 
