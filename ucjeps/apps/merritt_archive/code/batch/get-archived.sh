42da530ca1f721b8538dbd5503e2d97c372aae6c
John Lowe <jblowe@berkeley.edu>
2023-06-21 20:05:48 -0700

    CSW-817: fix typo in ucjeps image archiving helper script

--- ucjeps/apps/merritt_archive/code/batch/get-archived.sh
+++ ucjeps/apps/merritt_archive/code/batch/get-archived.sh
@@ -10,5 +10,5 @@ perl -pe "s/(\\\x[[:xdigit:]]{2})//eg;s/\\\\\\\/\\\/g;s/\\\'/'/g" x2 > x3
 cat x3 | jq -r '.[] | [.localID,.completionDate,.primaryID,.objectTitle]|@tsv' > x4
 cat x1 x4 all.archived.csv | sort -u > x5
 mv x5 all.archived.csv
-wc -1 all.archived.csv
+wc -l all.archived.csv
 rm x?
