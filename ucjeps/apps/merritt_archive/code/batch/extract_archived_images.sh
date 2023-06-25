de5842532ef23c69e49f55f285252bd1c1ad323d
John Lowe <jblowe@berkeley.edu>
2023-06-25 11:03:09 -0700

    CSW-817: refactor output of two ucjeps image archiving helper scripts

--- ucjeps/apps/merritt_archive/code/batch/extract_archived_images.sh
+++ ucjeps/apps/merritt_archive/code/batch/extract_archived_images.sh
@@ -2,4 +2,6 @@
 
 source step1_set_env.sh || { echo 'could not set environment vars. is step1_set_env.sh available?'; exit 1; }
 
-curl -S -s "${SOLR_SERVER}/solr/ucjeps-merritt/select?q=status_s%3Aarchived&fl=image_filename_s&rows=1000000" | jq -r '.response.docs[].image_filename_s'
+curl -S -s "${SOLR_SERVER}/solr/ucjeps-merritt/select?q=status_s%3Aarchived&fl=accession_number_s,image_filename_s,status_s,job_s,transaction_detail_s,transaction_date_dt&rows=1000000" |\
+  jq -r '.response.docs[] | [.accession_number_s,.image_filename_s,.status_s,.jobs_s,.transaction_date_dt,.transaction_detail_s]|@tsv' |\
+  sort -u
--- ucjeps/apps/merritt_archive/code/batch/get-archived.sh
+++ ucjeps/apps/merritt_archive/code/batch/get-archived.sh
@@ -4,11 +4,16 @@
 # and keeps a copy just in case
 
 grep 'jobState' /cspace/webapps_logs/ucjeps.webapps.log* > arc.json
-cat arc.json | grep -v 'could not'  | perl -ne 'print unless /job",$/' | perl -pe "s/^.*?{/{/;s/}'? :: .*/}/;" | perl -pe 's/job://g;' | jq -r '.[] | [.localID,.completionDate,.primaryID,.objectTitle]|@tsv' > x1
+cat arc.json | grep -v 'could not'  | perl -ne 'print unless /job",$/' | perl -pe "s/^.*?{/{/;s/}'? :: .*/}/;" |\
+  perl -pe 's/job://g;' |\
+  perl -pe "s/(\\\x[[:xdigit:]]{2})//eg;s/\\\\\\\/\\\/g;s/\\\'/'/g" |\
+  jq -r '.[] | [.localID,.completionDate,.primaryID,.objectTitle]|@tsv' \
+  > x1
 cat arc.json | grep 'could not'  | perl -pe "s/^.*?{/{/;s/}'? :: .*/}/;" | perl -pe 's/job://g;' > x2
 perl -pe "s/(\\\x[[:xdigit:]]{2})//eg;s/\\\\\\\/\\\/g;s/\\\'/'/g" x2 > x3
 cat x3 | jq -r '.[] | [.localID,.completionDate,.primaryID,.objectTitle]|@tsv' > x4
-cat x1 x4 all.archived.csv | sort -u > x5
-mv x5 all.archived.csv
-wc -l all.archived.csv
+cat x1 x4 archived.fromlogs.csv | perl -pe 's/-07:00\t/\t/' | sort -u > x5
+mv x5 archived.fromlogs.csv
+perl -pe '/^(.*?)\t/ && ($a = $1); $a =~ s/[_\.].*//; print "$a\t"; s/\t/\tarchived\t\t/' archived.fromlogs.csv | cut -f1-6 > archived.transactions.csv
+wc -l archived.*.csv
 rm x?
